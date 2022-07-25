687bfbfb76873fee44a41f0a1ea9183ce8ab5a19
Мария <glsip.girl1995@gmail.com>
2022-07-25 14:06:10 +0300

    Внесла правки

--- js/util.js
+++ js/util.js
@@ -1,46 +1,5 @@
 const ALERT_SHOW_TIME = 5000;
 
-// Возвращает случайное число в диапозоне, если не указано количество знаков после запятой, то возвращает целое
-const getRandomNumber = (min, max, decimalCount = 0) => {
-  let result;
-  try {
-    if (min === max) {
-      return min;
-    }
-    if (min > max) {
-      const tmp = max;
-      max = min;
-      min = tmp;
-    }
-    if (min < 0) {
-      min = min * -1;
-    }
-    if (max < 0) {
-      max = max * -1;
-    }
-    // Формула генерации взята с https://myrusakov.ru/js-random-numbers.html
-    const randomNumber = Math.random() * (max - min) + min;
-    result = randomNumber.toFixed(decimalCount);
-  } catch (error) {
-    result = 'Диапозон или количество знаков после запятой указаны некорректно';
-  }
-  return result;
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
-const getRandomArrayPart = (elements) => {
-  let start = getRandomNumber (0, elements.length - 1);
-  let end = getRandomNumber (0, elements.length - 1);
-  if (start > end) {
-    const tmp = end;
-    end = start;
-    start = tmp;
-  } else if ( start === end) {
-    return [elements[start]];
-  }
-  return elements.slice(start, end);
-};
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const RERENDER_DELAY = 500;
@@ -68,4 +27,4 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomNumber, getRandomArrayElement, getRandomArrayPart, isEscapeKey, showAlert, RERENDER_DELAY, FILE_TYPES};
+export {isEscapeKey, showAlert, RERENDER_DELAY, FILE_TYPES};
